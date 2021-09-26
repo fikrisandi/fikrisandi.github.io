877c9f87537b7cb7169662e5e411de730ffdb51b
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-09-27 00:17:56 +0700

    update Quiz 1

---
+++ quiz1/hometown/hometown.js
@@ -0,0 +1,73 @@
+const textDisplay = document.getElementById('text')
+const phrases = ['Description Of The City Of Gresik and Its Nickname', 'Happy Reading']
+let i = 0
+let j = 0
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop() {
+    isEnd = false
+    textDisplay.innerHTML = currentPhrase.join('')
+
+    if (i < phrases.length) {
+
+        if (!isDeleting && j <= phrases[i].length) {
+            currentPhrase.push(phrases[i][j])
+            j++
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (isDeleting && j <= phrases[i].length) {
+            currentPhrase.pop(phrases[i][j])
+            j--
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (j == phrases[i].length) {
+            isEnd = true
+            isDeleting = true
+        }
+
+        if (isDeleting && j === 0) {
+            currentPhrase = []
+            isDeleting = false
+            i++
+            if (i === phrases.length) {
+                i = 0
+            }
+        }
+    }
+    const spedUp = Math.random() * (80 - 50) + 50
+    const normalSpeed = Math.random() * (300 - 200) + 200
+    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+    setTimeout(loop, time)
+}
+
+loop()
+
+document.addEventListener("scroll", handleScroll);
+// get a reference to our predefined button
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+
+function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.5;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > GOLDEN_RATIO) {
+    //show button
+    scrollToTopBtn.style.display = "block";
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
