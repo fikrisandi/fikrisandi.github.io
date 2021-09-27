aae8184e31f0e568209246222fac995f3f2fac67
Muhammad Fikri Sandi Pratama <55092974+fikrisandi@users.noreply.github.com>
2021-09-27 14:23:17 +0700

    Update animation

--- quiz1/tourist/tourist.js
+++ quiz1/tourist/tourist.js
@@ -23,6 +23,54 @@ open.addEventListener("click", function(){
     }
 });
 
+const textDisplay = document.getElementById('text')
+const phrases = ['Gresik Tourist PLace','You Can View The Map If You Want To Visit It','Good Luck :)']
+let i = 0
+let j = 0
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop() {
+    isEnd = false
+    textDisplay.innerHTML = currentPhrase.join('')
+
+    if (i < phrases.length) {
+
+        if (!isDeleting && j <= phrases[i].length) {
+            currentPhrase.push(phrases[i][j])
+            j++
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (isDeleting && j <= phrases[i].length) {
+            currentPhrase.pop(phrases[i][j])
+            j--
+            textDisplay.innerHTML = currentPhrase.join('')
+        }
+
+        if (j == phrases[i].length) {
+            isEnd = true
+            isDeleting = true
+        }
+
+        if (isDeleting && j === 0) {
+            currentPhrase = []
+            isDeleting = false
+            i++
+            if (i === phrases.length) {
+                i = 0
+            }
+        }
+    }
+    const spedUp = Math.random() * (80 - 50) + 50
+    const normalSpeed = Math.random() * (300 - 200) + 200
+    const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+    setTimeout(loop, time)
+}
+
+loop()
+
 document.addEventListener("scroll", handleScroll);
 // get a reference to our predefined button
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
